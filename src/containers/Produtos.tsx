0532d427a5aade408d1963554224e579537473b0
Leandro Walter <leandromarqueswalter@gmail.com>
2024-01-08 17:43:42 -0300

    Deus Abençoe

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,35 @@
+import { useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
 
 import * as S from './styles'
+import { RootReducer } from '../store'
+import { useGetProdutosQuery } from '../services/api'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favoritos.itens
+  )
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    const IdsDosFavoritos = itensFavoritos.map((f) => f.id)
 
     return IdsDosFavoritos.includes(produtoId)
   }
 
+  if (isLoading) return <h2>Carregando...</h2>
+
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        const favoritosSemProduto = state.itens.filter(
+          (p) => p.id !== produto.id
+        )
+        state.itens = favoritosSemProduto
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
